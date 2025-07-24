e7a6f7475a8b26bbe1bb8085651f22eaec0337d5
Ronald Rosete <ronaldtorresrosete30@gmail.com>
2025-07-24 18:49:47 +0800

    Update 03_prepare_cmake.sh

--- Packages/03_prepare_cmake.sh
+++ Packages/03_prepare_cmake.sh
@@ -1,2 +1,8 @@
+#!/bin/bash
+
+# This script prepare the CMake needed for compiling the Raylib.
+# Current script downloads CMake Version 3.29.3.
+# Current Raylib code needs CMake with version 3.2 and up.
+
 wget https://github.com/Kitware/CMake/releases/download/v3.29.3/cmake-3.29.3-linux-x86_64.tar.gz
 tar -xzf cmake-3.29.3-linux-x86_64.tar.gz
